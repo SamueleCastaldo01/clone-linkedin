3fdb65675e1708d9f0f8c8001aba801b744bbdba
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-09 12:45:19 +0200

    commit

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import NavComponent from "../components/NavComponet";
-=======
-import TabProfile from "../components/TabProfile";
-import MySidebar from "../components/MyProfileSidebar";
->>>>>>> master
 
 function Profile() {
   return (
     <>
-<<<<<<< HEAD
     <div className="App">
       <header className="App-header"><NavComponent/></header>
       <main>
@@ -16,19 +10,6 @@ function Profile() {
         <div className="row">
           <div className="col-8 bg-black">
             <h2>Ciao</h2>
-=======
-      <div className="App">
-        <header className="App-header"></header>
-        <main>
-          <h2>Ciao</h2>
-          <div className="row">
-            <div className="col-8 p-0">
-              <TabProfile />
-            </div>
-            <div className="col-4">
-                <MySidebar />
-            </div>
->>>>>>> master
           </div>
         </main>
         <footer></footer>
