8fd3d5752cca4201a1db4fdc62cddae73a997da7
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-09 10:35:30 +0200

    commit struttura

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+          <h2>Ciao</h2>
+          <div className='row'>
+            <div className='col-8 bg-black'>
+                <h2>Ciao</h2>
+            </div>
+            <div className='col-4 bg-body-secondary'>
+                <h3>Cosa</h3>
+            </div>
+          </div>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
