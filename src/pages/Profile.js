1799d190c74f33ea8265fe6a10a5e2249c1ad94d
Marco Zagaria <marcozagaria@live.com>
2024-09-09 13:13:24 +0200

    merge

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,3 @@
-import MyHomeSIdebar from "../components/MyHomeSidebar";
-import MyProfileSidebar from "../components/MyProfileSidebar";
 import TabProfile from "../components/TabProfile";
 import MySidebar from "../components/MyProfileSidebar";
 import NavComponent from "../components/NavComponent";
@@ -8,18 +6,17 @@ function Profile() {
   return (
     <>
       <div className="App">
-        <header className=""><NavComponent /></header>
+        <header className="">
+          <NavComponent />
+        </header>
         <main>
           <h2>Ciao</h2>
           <div className="row">
             <div className="col-8 p-0">
               <TabProfile />
             </div>
-            <div className="col-4 bg-body-secondary">
-              <MyProfileSidebar />
-              <MyHomeSIdebar />
             <div className="col-4">
-                <MySidebar />
+              <MySidebar />
             </div>
           </div>
         </main>
