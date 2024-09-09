a034ee69d7d4184f34b1dca9fc1ba1e2a3f72cb2
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-09 12:19:16 +0200

    commit

--- src/components/NavComponet.jsx
+++ src/components/NavComponet.jsx
@@ -1,10 +1,12 @@
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { TextField, InputAdornment } from "@mui/material"; // Importa TextField e InputAdornment
 import {
+  AppsRounded,
   ChatBubbleRounded,
   GroupRounded,
   HomeRounded,
   NotificationsRounded,
+  Person2Rounded,
   Search,
   WorkRounded,
 } from "@mui/icons-material";
@@ -45,18 +47,35 @@ const NavComponent = () => {
         <Nav className="mx-auto">
           <Nav.Link href="#home">
             <HomeRounded fontSize="large" />
+            <div className="fs-6">Home</div>
           </Nav.Link>
           <Nav.Link href="#features">
             <GroupRounded fontSize="large" />
+            <div className="fs-6">Rete</div>
           </Nav.Link>
           <Nav.Link href="#pricing">
             <WorkRounded fontSize="large" />
+            <div className="fs-6">Lavoro</div>
           </Nav.Link>
           <Nav.Link href="#pricing">
             <ChatBubbleRounded fontSize="large" />
+            <div className="fs-6">Messagistica</div>
           </Nav.Link>
           <Nav.Link href="#pricing">
             <NotificationsRounded fontSize="large" />
+            <div className="fs-6">Notifiche</div>
+          </Nav.Link>
+        </Nav>
+      </Container>
+      <Container fluid>
+        <Nav className="ms-auto">
+          <Nav.Link href="#pricing">
+            <Person2Rounded fontSize="large" />
+            <div className="fs-6">Tu</div>
+          </Nav.Link>
+          <Nav.Link href="#pricing">
+            <AppsRounded fontSize="large" />
+            <div className="fs-6">Per le aziende</div>
           </Nav.Link>
         </Nav>
       </Container>
