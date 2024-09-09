e2574733dce6dfd518a18a6e67053149fb30af37
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-09 12:37:38 +0200

    commit

--- src/components/NavComponet.jsx
+++ src/components/NavComponet.jsx
@@ -13,67 +13,58 @@ import {
 
 const NavComponent = () => {
   return (
-    <Navbar bg="light" data-bs-theme="light">
-      <Container fluid className="px-5">
+    <Navbar bg="light" data-bs-theme="light" className="px-5">
+      <Container fluid className="d-flex justify-content-between align-items-center px-5">
         <Navbar.Brand href="#home">
           <img
             src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000"
             alt="logoLinkedin"
           />
         </Navbar.Brand>
-        {/* <TextField
-      variant="outlined"
-      placeholder="Search"
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <Search />
-          </InputAdornment>
-        ),
-      }}
-    /> */}
+
         <TextField
+          className="flex-grow-1 mx-4"
           placeholder="Search"
-          slotProps={{
-            input: {
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Search />
-                </InputAdornment>
-              ),
-            },
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
           }}
         />
-        <Nav className="mx-auto">
-          <Nav.Link href="#home">
+
+    
+        <Nav className="d-flex align-items-center">
+          <Nav.Link href="#home" className="text-center">
             <HomeRounded fontSize="large" />
             <div className="fs-6">Home</div>
           </Nav.Link>
-          <Nav.Link href="#features">
+          <Nav.Link href="#features" className="text-center">
             <GroupRounded fontSize="large" />
             <div className="fs-6">Rete</div>
           </Nav.Link>
-          <Nav.Link href="#pricing">
+          <Nav.Link href="#pricing" className="text-center">
             <WorkRounded fontSize="large" />
             <div className="fs-6">Lavoro</div>
           </Nav.Link>
-          <Nav.Link href="#pricing">
+          <Nav.Link href="#chat" className="text-center">
             <ChatBubbleRounded fontSize="large" />
-            <div className="fs-6">Messagistica</div>
+            <div className="fs-6">Messaggistica</div>
           </Nav.Link>
-          <Nav.Link href="#pricing">
+          <Nav.Link href="#notifications" className="text-center">
             <NotificationsRounded fontSize="large" />
             <div className="fs-6">Notifiche</div>
           </Nav.Link>
         </Nav>
-      </Container>
-      <Container fluid>
-        <Nav>
-          <Nav.Link href="#pricing">
+
+    
+        <Nav className="d-flex align-items-center ms-auto">
+          <Nav.Link href="#profile" className="text-center">
             <Person2Rounded fontSize="large" />
             <div className="fs-6">Tu</div>
           </Nav.Link>
-          <Nav.Link href="#pricing">
+          <Nav.Link href="#apps" className="text-center">
             <AppsRounded fontSize="large" />
             <div className="fs-6">Per le aziende</div>
           </Nav.Link>
@@ -81,6 +72,7 @@ const NavComponent = () => {
       </Container>
     </Navbar>
   );
+    
 };
 
 export default NavComponent;
