0ce7695b1adde94d2f2d3be9dac47bfc1fa92dd3
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-10 10:34:21 +0200

    commit

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,8 +3,9 @@ import AddIcon from "@mui/icons-material/Add";
 import EditIcon from "@mui/icons-material/Edit";
 import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
+import Button from '@mui/material/Button';
 import Form from "react-bootstrap/Form";
+import TextField from '@mui/material/TextField';
 
 function Experience() {
   const [show, setShow] = useState(false);
@@ -68,18 +69,11 @@ function Experience() {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Modal Experience</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
+          <form onSubmit={""}>
+          <TextField id="outlined-basic" label="Role" variant="outlined" className="w-100"/>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
@@ -87,15 +81,10 @@ function Experience() {
               <Form.Label>Example textarea</Form.Label>
               <Form.Control as="textarea" rows={3} />
             </Form.Group>
-          </Form>
+          </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+        <Button variant="contained" className="rounded-4"  onClick={handleClose}>Salva</Button>
         </Modal.Footer>
       </Modal>
     </>
