57da043ffd3763b39fc47d55f084bca2d475a573
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-11 15:50:55 +0200

    commit

--- src/components/JobsSection.jsx
+++ src/components/JobsSection.jsx
@@ -1,12 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import moment from "moment";
 
 const JobSection = function () {
 
-
-
+  const jobs = useSelector((state) => state.jobs.jobs);
+  
   return (
-  <>
+    <>
+      <div className="bg-white rounded-4 position-relative tabPro mt-3 p-4">
+        <div className="d-flex align-items-center justify-content-between mb-3">
+          <h5 className="fw-bold m-0">Offerte di lavoro per te</h5>
+        </div>
 
-  </>
+        {jobs.length > 0 ? (
+          jobs.map((experience, index) => (
+            <div
+              key={index}
+              className="d-flex align-items-center justify-content-between mb-4"
+            >
+              <div className="d-flex align-items-center">
+                <div className="me-2">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuy8Th0qZPzQUtjChGa8fvmoGeCdmk9mtpWg&s"
+                    style={{ width: "50px", height: "50px" }}
+                    alt="company"
+                  />
+                </div>
+                <div>
+                  <h6 className="m-0 fw-bold">{experience.role}</h6>
+                  <p className="m-0">{experience.area}</p>
+                  <p className="m-0">
+                    {moment(experience.startDate).format("DD/MM/YY")} -
+                    {experience.endDate
+                      ? moment(experience.endDate).format("DD/MM/YY")
+                      : "Presente"}
+                  </p>
+                  <p className="m-0">{experience.company}</p>
+                </div>
+              </div>
+              <div>
+
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Nessuna esperienza trovata</p>
+        )}
+      </div>
+    </>
   );
 };
 
