ccc02e13bf9abeac6d14f6e0606c55819212eba5
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-11 09:59:51 +0200

    commit

---
+++ src/redux/reducers/postsReducer.js
@@ -0,0 +1,27 @@
+import { FETCH_POSTS, POSTS_ERROR } from "../actions/types"
+
+
+const initialState = {
+    posts: [],
+    error: null
+}
+
+export const postReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_POSTS:
+            return {
+                ...state,
+                post: action.payload,
+                error: null
+            }
+
+        case POSTS_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+            }
+
+            default: 
+            return state
+    }
+}
