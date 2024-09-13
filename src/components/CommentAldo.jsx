2e5606e503336cbb94fbf8a8f7306b1709d0cf2c
FedericoIafolla <iafollafederico@gmail.com>
2024-09-13 09:41:45 +0200

    Update CommentAldo.jsx

--- src/components/CommentAldo.jsx
+++ src/components/CommentAldo.jsx
@@ -154,7 +154,6 @@ const CommentAldo = () => {
                 <StarIcon style={{ color: "#FFD700" }} />{" "}
                 {/* Colore giallo per le stelle */}
               </div>
-              s
             </div>
           ))}
         </div>
