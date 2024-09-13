a901dffee1c34f65e719426cb264ce231bb516f1
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-13 11:29:30 +0200

    commit

---
+++ src/components/Annuncio.js
@@ -0,0 +1,10 @@
+export function Annuncio() {
+  return (
+    <>
+        <div>
+            <img className=" img-fluid rounded-4 cardOmbra" src="ads.png"/>
+        </div>
+      
+    </>
+  )
+}
